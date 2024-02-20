77182f6556a75186712c2a124216d76bdf9f9972
Seny Irinarkhova <kseniiaIrinarkhova@gmail.com>
2024-02-19 17:20:10 -0800

    added middleware

--- server.js
+++ server.js
@@ -1,10 +1,31 @@
 const express = require("express");
+const responseTime = require('response-time');
 
 const app = express();
 
 const PORT = 3000;
 
 
+//Middleware
+app.use(responseTime());
+
+
+
+// error handler
+app.use((err, req, res, next) => {
+    res.status(400).send(err.message);
+});
+//routes
+
+app.get("/", (req,res)=>{
+    res.send("Hello Express!")
+})
+
+
+//listening
 app.listen(PORT, ()=>{
     console.log(`Server is listening on port: ${PORT}.`)
 });
+
+
+//helper functions:
