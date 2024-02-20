8272574e2787564c0dea9d581790d6513613bd88
Seny Irinarkhova <kseniiaIrinarkhova@gmail.com>
2024-02-19 19:37:13 -0800

    comments added

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
+//Import resources
+//Express
 const express = require("express");
+//Response-time
 const responseTime = require('response-time');
+//Pug view engine
 const pug = require("pug");
 
-const fs = require("fs");
-
-
 const app = express();
-
 const PORT = 3000;
 
 
 //Middleware
+//added response time to header
 app.use(responseTime());
+//added ability to see some static files
 app.use(express.static('./static'))
 
 
@@ -21,10 +23,12 @@ app.use((err, req, res, next) => {
 });
 //routes
 
+//main route
 app.get("/", (req,res)=>{
     res.send(pug.renderFile('./view/index.pug', { pageTitle: 'Express App' }))
 })
 
+//route with user data
 app.get('/:name/:favNumber', (req,res)=>{
     const user = req.params.name
     const num = req.params.favNumber
@@ -32,6 +36,7 @@ app.get('/:name/:favNumber', (req,res)=>{
     Array.prototype.forEach.call(num, (digit) => {
         sum += parseInt(digit)
     });
+
     res.send(pug.renderFile('./view/user.pug', { 
         pageTitle: `${user} Fansy Page`,
         userName: user,
@@ -41,16 +46,12 @@ app.get('/:name/:favNumber', (req,res)=>{
 
 })
 
+//route for picture downloading
 app.get('/pugpic',(req,res)=>{
-    console.log("It works!")
     res.download('./static/pug.png', 'pug.png')
-    // res.sendStatus(200)
 })
 
 //listening
 app.listen(PORT, ()=>{
     console.log(`Server is listening on port: ${PORT}.`)
 });
-
-
-//helper functions:
--- static/client.js
+++ static/client.js
@@ -1,5 +1,8 @@
-const downloadBtn = document.getElementById('download');
+const downloadBtn = document.getElementById('download'); //find the button
+
+//add event listener
 downloadBtn.addEventListener('click', (event)=>{
     event.preventDefault()
+    //go to another page for downloading picture
     window.location.href = '/pugpic'
 })
